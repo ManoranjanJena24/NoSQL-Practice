b8a8ecce7ec1231b06cf7a3e239b02188307d2cb
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-04-08 16:37:30 +0530

    Adding Product Using Mongoose

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
 const errorController = require('./controllers/error');
-const mongoConnect=require('./util/database').mongoConnect
-const User=require('./models/user')
+// const User = require('./models/user')
 
 const app = express();
 
@@ -18,21 +18,24 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById("661389ce911ba2ebf9453d71")
-    .then(user => {
-        req.user =new User(user.name,user.email,user.cart,user._id);
-        next();
-    })
-        .catch(err => console.log(err));
-   
-});
+// app.use((req, res, next) => {
+//     User.findById("661389ce911ba2ebf9453d71")
+//         .then(user => {
+//             req.user = new User(user.name, user.email, user.cart, user._id);
+//             next();
+//         })
+//         .catch(err => console.log(err));
+
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-mongoConnect(() => {
-    app.listen(3000)
-})
+mongoose.connect('mongodb+srv://Sharpner:sharpner123@cluster0.hvfvrv3.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(() => {
+        app.listen(3000)
+    }).catch((err) => {
+        console.log(err)
+    })
 
