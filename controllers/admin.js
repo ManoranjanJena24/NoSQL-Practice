2fb4d4ede27b862472149213aa895b8386323ac2
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-04-08 17:16:35 +0530

    Edit and Delete

--- controllers/admin.js
+++ controllers/admin.js
@@ -60,8 +60,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
   
-     const product=new Product(updatedTitle,updatedPrice,updatedDesc,updatedImageUrl,prodId)
-    product.save().then(result => {
+  Product.findById(prodId).then((product) => {
+    product.title = updatedTitle
+    product.price = updatedPrice
+    product.imageUrl = updatedImageUrl
+    product.description=updatedDesc
+    return product.save() 
+    })
+   .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
@@ -69,7 +75,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -82,7 +88,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndDelete(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
