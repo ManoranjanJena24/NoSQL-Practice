6045854d0841e0ce3717a0005483a0caa0bf631b
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-04-08 12:35:56 +0530

    Relation between Cart and Users

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById("661389ce911ba2ebf9453d71")
     .then(user => {
-        req.user = user;
+        req.user =new User(user.name,user.email,user.cart,user._id);
         next();
     })
         .catch(err => console.log(err));
